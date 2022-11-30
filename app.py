3ac68194cb7bacbc2d0052a7b17e4f089e66b740
Sandeep01062002 <119503516+Sandeep01062002@users.noreply.github.com>
2022-11-30 18:21:57 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+st.write("
+         # TDS Module 8")
+a=st.number_input("Enter first number")
+b=st.number_input("Enter second number") 
+c=a-b
+st.write("Difference of given number is",c)
+st.write("By SANDEEP KUMAR SHARMA 21F1004425")
